fc59a38847a14c0afa264eee6631fe906c0c757e
root <matthew.lopez1377@gmail.com>
2024-07-14 05:55:59 -0500

    Rearranged some stuff

--- sqlite/queries/contacts.py
+++ sqlite/queries/contacts.py
@@ -1,16 +1,41 @@
 import sqlite3 as sq
 
-
 #3. Create a python file for modifying contacts table (queries/contacts.py)
 #    - Add function for creating contacts
 #    - Add function for modifying contacts
 
-
-
-
-def create_contact(first_name, last_name, email, phone, contact_address, connection):
-
+# Description:
+#       Checks to see if the contact exists
+#
+# Pre-Conditions:
+#       contact_email (string) 
+#       connections (sqlite connection object)
+#
+# Post:
+#       returns a bool to check if the contact exists
+def contact_exist(contact_email, connection):
+    
     cursor = connection.cursor()
+
+    # Query for email
+    query = '''
+        SELECT email
+        FROM contacts
+        WHERE email == ?
+    '''
+
+    # Execute the query
+    sq.execute(query, (contact_email,))
+
+    # Fetch results of the query
+    results = cursor.fetchall()
+
+    # If there is no results, return false, else true
+    if len(results) == 0:
+        return False
+    else:
+        return True
+    
 
     # Query to check if the contact already is in database using EXISTS
     query = '''
@@ -46,6 +71,18 @@ def create_contact(first_name, last_name, email, phone, contact_address, connect
 
     # Check if the results is empty, if empty contact doesn't exists
     if not len(results) == 0:
+        return False
+    else:
+        return True
+
+
+
+def create_contact(first_name, last_name, email, phone, contact_address, connection):
+
+    cursor = connection.cursor()
+
+    # Check if the results is empty, if empty contact doesn't exists
+    if contact_exist(email, connection):
         return (False, "contact_exists")
     else:
 
@@ -60,6 +97,15 @@ def create_contact(first_name, last_name, email, phone, contact_address, connect
                 )
             VALUES (?, ?, ?, ?, ?)
         '''
+        
+        # Pack data into tuple
+        data = (
+            first_name,
+            last_name,
+            email,
+            phone,
+            contact_address
+        )
 
         # Insert the new contact and commit
         cursor.execute(query, (data,))
@@ -67,3 +113,4 @@ def create_contact(first_name, last_name, email, phone, contact_address, connect
 
 
 def modify_contact(first_name, last_name, email, phone, contact_address, connection):
+    pass
